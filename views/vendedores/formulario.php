4efd2b3772d819f57caae8f0c532cc040a54d3dd
Juan Cruz <juan_pineda_12@hotmail.es>
2023-07-03 14:11:48 -0300

    commit-end

--- views/vendedores/formulario.php
+++ views/vendedores/formulario.php
@@ -5,16 +5,16 @@
     <input type="file" name="vendedor[imagen]" id="imagen" accept="image/jpeg, image/png">
 
     <?php if($vendedor->imagen) { ?>
-        <img src="/imagenes/<?php echo $vendedor->imagen ?>" class="imagen-actu">
+        <img src="/imagenes/<?php echo trim($vendedor->imagen); ?>" class="imagen-actu">
     <?php } ?>
 
     <label for="nombre">Nombre</label>
-    <input type="text" name="vendedor[nombre]" id="nombre" placeholder="Nombre Vendedor/a" value="<?php echo s($vendedor->nombre); ?>">
+    <input type="text" name="vendedor[nombre]" id="nombre" placeholder="Nombre Vendedor/a" value="<?php echo trim(s($vendedor->nombre)); ?>">
 
     <label for="apellido">Apellido</label>
-    <input type="text" name="vendedor[apellido]" id="apellido" placeholder="Apellido Vendedor/a" value="<?php echo s($vendedor->apellido); ?>">
+    <input type="text" name="vendedor[apellido]" id="apellido" placeholder="Apellido Vendedor/a" value="<?php echo trim(s($vendedor->apellido)); ?>">
 
     <label for="telefono">Telefono</label>
-    <input type="number" name="vendedor[telefono]" id="telefono" placeholder="Telefono Vendedor/a" value="<?php echo s($vendedor->telefono); ?>">
+    <input type="number" name="vendedor[telefono]" id="telefono" placeholder="Telefono Vendedor/a" value="<?php echo trim(s($vendedor->telefono)); ?>">
 
 </fieldset>
